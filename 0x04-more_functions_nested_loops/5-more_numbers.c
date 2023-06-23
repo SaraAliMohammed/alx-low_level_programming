22beba194112d2199d29b063b30d74c474eeccdd
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-06-22 20:07:31 -0700

    Task completed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,19 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, num;
 
 	for (j = 0; j <= 9; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar(i + '0');
+			num = i;
+			if (i > 9)
+			{
+				_putchar(1 + '0');
+				num = i % 10;
+			}
+			_putchar(num + '0');
 		}
 	}
 	_putchar('\n');
