06e2fdf362e9967cb81197784d150dd120d39b72
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-06-22 19:23:00 -0700

    Task3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: 0 on Success
+*/
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	do {
+		_putchar(i + '0');
+		i++;
+	} while (i >= 0 && i <= 9)
+	_putchar('\n');
+}
