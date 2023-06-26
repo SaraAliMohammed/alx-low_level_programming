f3c6d63b2bf93cf589e309860c62d384482b4cd3
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-06-26 04:30:51 -0700

    Task completed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 
 	for (len = 0; str[len] != '\0'; len++)
 		;
-	for (i = 0; i <= (len - 1) / 2; i++)
+	for (i = (len - 1) / 2; i ! < len; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
