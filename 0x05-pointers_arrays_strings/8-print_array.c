9d87630167818291db98f18a9d18bae2e3da6973
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-06-26 07:33:50 -0700

    Task completed

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,13 +14,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i] + 0);
-		if (i != n - 1)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
 		else
-			_putchar('\n');
+			printf("%d\n", a[i]);
 	}
 }
