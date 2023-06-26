c1b39a15658829f85d3a1fa98d173f053b36f604
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-06-26 03:31:24 -0700

    Task completed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a string, in reverse.
+ * @s: string to be printed.
+ *
+ * Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+	int i, length;
+
+	length = _strlen(*s);
+	for (i = length - 1; i > 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
