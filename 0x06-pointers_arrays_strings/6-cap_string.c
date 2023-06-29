9a65234f1383d4e9d6c06fbcb58a76d70a7b94b9
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-06-29 02:41:26 -0700

    Task completed

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * isLower - test if character is lowercase.
+ * @ch: character parameter.
+ *
+ * Return: 1 if lowercase, 0 if not.
+*/
+
+int isLower(char ch)
+{
+	return (ch >= 97 && ch <= 122);
+}
+
+/**
+ * isDelimiter - test if the chracter is delimiter.
+ * @ch: character parameter.
+ *
+ * Return: 1 if delimiter, 0 if not.
+*/
+
+int isDelimiter(char ch)
+{
+	int i;
+	char delimiters[] = " \t\n,.!?\"(){}";
+
+	for (i = 0; i < 12; i++)
+		if (ch == delimiters[i])
+			return (1);
+	return (0);
+}
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: string parameter.
+ *
+ * Return: string capitalized.
+*/
+
+char *cap_string(char *str)
+{
+	char *ptr = str;
+	int foundDelimiter = 1;
+
+	while (*str)
+	{
+		if (isDelimiter(*str))
+			foundDilemiter = 1;
+		else if (isLower(*str) && foundDilemiter)
+		{
+			*str -= 32;
+			foundDilemiter = 0;
+		}
+		else
+			foundDilemiter = 0;
+		str++;
+	}
+	return (str);
+}
