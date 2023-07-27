5162dc62e5a7d13d231d9c99281291d4f493bdc8
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-07-27 14:39:07 -0700

    Task completed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * beforeMain - function executed before main
+ *
+ * Return: Nothing.
+ */
+
+void __attribute__ ((constructor)) beforeMain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
