14864511f09283e3c136a90b1dfe8f5b446bc983
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-10-12 02:17:58 -0700

    Task2_Edit

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,16 +16,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (new ? free(new), NULL : NULL);
 	new->n = n;
 	new->prev = NULL;
-	if (!*head)
-	{
-		*head = new;
-		new->next = NULL;
-	}
-	else
-	{
-		new->next = *head;
+	new->next = *head;
+	if (*head != NULL)
 		(*head)->prev = new;
-		*head = new;
-	}
+	*head = new;
 	return (new);
 }
