26ca15dd8dc8c608c2f4dce23c72df23aea683aa
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-03 08:46:43 -0700

    Task4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: The hash table you want to look into.
+ * @key: The key you are looking for.
+ *
+ * Return: The value associated with the element,
+ *	or NULL if key couldn’t be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, i;
+
+	if (!ht || !key)
+		return (NULL);
+	index = key_index((const unsigned char *) key, ht->size);
+	for (i=index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			return ht->array[i]->value;
+		}
+	}
+	return (NULL);
+}
