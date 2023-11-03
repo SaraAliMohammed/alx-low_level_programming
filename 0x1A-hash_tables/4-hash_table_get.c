0c1772f5a06afd1b347bd068f554091e01dbb173
SaraAliMohammed <sara.ali.mohammed213@gmail.com>
2023-11-03 08:48:27 -0700

    Task4_Edit betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -15,12 +15,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	if (!ht || !key)
 		return (NULL);
 	index = key_index((const unsigned char *) key, ht->size);
-	for (i=index; ht->array[i]; i++)
+	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
-		{
-			return ht->array[i]->value;
-		}
+			return (ht->array[i]->value);
 	}
 	return (NULL);
 }
